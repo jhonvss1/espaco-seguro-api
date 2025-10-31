37b519eeca075140703b2e3a32076187c877c215
Victor Fonseca <pvictorsf07@outlook.com>
2025-10-31 11:24:12 -0300

    feature: create folders by DDD and DbContext

---
+++ espaco-seguro-api/4 - Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace espaco_seguro_api._4___Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {}
+
+        // Defina os DbSets para suas entidades aqui
+    }
+}
