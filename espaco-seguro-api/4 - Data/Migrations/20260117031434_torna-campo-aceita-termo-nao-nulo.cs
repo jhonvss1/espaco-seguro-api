3400052d3f3b3f8a3f29189b0fcaec67b02f067e
JhonSantos <jhonsilva.vs1@gmail.com>
2026-02-25 20:39:07 -0300

    feat: adiciona validações para segurança

---
+++ espaco-seguro-api/4 - Data/Migrations/20260117031434_torna-campo-aceita-termo-nao-nulo.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace espaco_seguro_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class tornacampoaceitatermonaonulo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "aceitou_termos",
+                table: "usuario",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "aceitou_termos",
+                table: "usuario",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+    }
+}
